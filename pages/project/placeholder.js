908d019e013897f75eae9c835918933f7b7118d5
Zahid <zahid@zmdev.com>
2021-12-20 18:45:25 +0000

    placeholder project page with hero

---
+++ pages/project/placeholder.js
@@ -0,0 +1,12 @@
+import Hero from "@/components/project/hero";
+import Navbar from "@/components/shared/navbar";
+
+export default function ProjectDetails() {
+    return (
+        <div className="w-full">
+            <Navbar />
+
+            <Hero />
+        </div>
+    )
+}
