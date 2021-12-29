03f86c7e0d905cea314eeed833d354ce7c6f6e88
Zahid <zahid@zmdev.com>
2021-12-29 17:53:41 +0000

    removed red background

--- components/project/campaign-details/index.js
+++ components/project/campaign-details/index.js
@@ -4,7 +4,7 @@ import Story from "./story";
 
 export default function CampaignDetails() {
     return (
-        <div className="w-full bg-red-100">
+        <div className="w-full">
             <div className="w-full max-w-5xl mx-auto px-4 flex flex-row py-4 space-x-4">
                 <div className="w-3/12 flex flex-col space-y-4">
                     <Menu />
