e21bbabc1a39327299a720d9cf9c88e18f7629f3
Zahid <zahid@zmdev.com>
2022-01-13 15:27:58 +0000

    login method on backend provider

--- context/backend/index.js
+++ context/backend/index.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { makeBackendActor, makeBackendActorWithIdentity } from "@/ui/service/actor-locator";
 
 const INITIAL_STATE = {
     backend: null,
@@ -11,7 +12,43 @@ export const BackendContext = createContext(INITIAL_STATE);
 export function BackendProvider({ children, backend }) {
     const [_backend, setBackend] = useState(backend || INITIAL_STATE.backend);
 
-    const value = { backend: _backend, setBackend };
+    const login = async () => {
+        const environmentName = process.env.NEXT_PUBLIC_ENVIRONMENT;
+
+        let backend;
+
+        if (environmentName === "development") {
+            backend = makeBackendActor();
+        }
+
+        if (environmentName !== "development") {
+            backend = await authClientLogin();
+        }
+
+        setBackend(backend);
+    }
+
+    const authClientLogin = async () => {
+        const authClient = await AuthClient.create();
+
+        return new Promise((resolve, reject) => {
+            authClient.login({
+                onSuccess: async () => {
+                    const identity = authClient.getIdentity();
+
+                    if (!identity) {
+                        return reject(new Error("identity is null"));
+                    }
+
+                    const backend = makeBackendActorWithIdentity(identity);
+
+                    resolve(backend)
+                }
+            })
+        });
+    }
+
+    const value = { backend: _backend, login };
 
     return (
         <BackendContext.Provider value={value}>
