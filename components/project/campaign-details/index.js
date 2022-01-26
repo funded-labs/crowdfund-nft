701d64a8e857982c330c02ee12d3c853c50c6170
Zahid <zahid@zmdev.com>
2022-01-26 21:22:08 +0000

    story skeleton

--- components/project/campaign-details/index.js
+++ components/project/campaign-details/index.js
@@ -2,7 +2,24 @@ import AuthorProfile from "./author-profile";
 import Menu from "./menu";
 import Story from "./story";
 
-export default function CampaignDetails() {
+export default function CampaignDetails({ isLoading }) {
+    if (isLoading) {
+        return (
+            <div className="w-full">
+                <div className="w-full max-w-5xl mx-auto px-4 flex flex-row py-4 space-x-4">
+                    <div className="w-3/12 flex flex-col space-y-4">
+                        <Menu />
+                        <AuthorProfile />
+                    </div>
+    
+                    <div className="w-9/12 flex flex-col">
+                        <Story isLoading={true} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="w-full">
             <div className="w-full max-w-5xl mx-auto px-4 flex flex-row py-4 space-x-4">
--- components/project/campaign-details/story.js
+++ components/project/campaign-details/story.js
@@ -1,4 +1,23 @@
-export default function Story() {
+export default function Story({ isLoading }) {
+    if (isLoading) {
+        return (
+            <div className="w-full shadow p-4 rounded-2xl bg-white flex flex-col space-y-2 items-start">
+                <div className="w-16 h-6 rounded bg-gray-200 animate-pulse" />
+    
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-7/12" />
+
+                <div className="py-5" />
+
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-4/12" />
+            </div>
+        );
+    }
+
     return (
         <div className="w-full shadow p-4 rounded-2xl bg-white flex flex-col items-start">
             <p className="font-bold text-base text-black">
