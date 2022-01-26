8389fe497354e095bdc42cd875f6db1aee4a6282
Zahid <zahid@zmdev.com>
2022-01-26 21:16:14 +0000

    skeleton for hero

--- components/project/hero/index.js
+++ components/project/hero/index.js
@@ -1,4 +1,51 @@
-export default function Hero() {
+export default function Hero({ isLoading }) {
+
+    if (isLoading) {
+        return (
+            <section className="w-full bg-white">
+                <div className="w-full max-w-5xl mx-auto flex flex-col px-4 py-5">
+                    <div className="animate-pulse bg-gray-200 h-14 w-96 rounded mb-3" />
+                    <div className="w-full flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-8">
+                        <div className="w-full lg:w-7/12 flex flex-col">
+                            <figure className="w-full h-96 bg-gray-200 animate-pulse rounded-xl mb-1" />
+                        </div>
+    
+                        <div className="w-full lg:w-5/12 flex flex-col">
+                            <div className="h-3 bg-gray-200 animate-pulse rounded-full relative overflow-hidden" />
+                            <div className="w-full flex flex-col py-3">
+                                <div className="bg-gray-200 h-10 animate-pulse w-20 rounded mb-2" />
+                                <div className="bg-gray-200 h-6 animate-pulse w-40 rounded" />
+                            </div>
+                            <div className="w-full flex flex-col py-3">
+                                <div className="bg-gray-200 h-10 animate-pulse w-20 rounded mb-2" />
+                                <div className="bg-gray-200 h-6 animate-pulse w-40 rounded" />
+                            </div>
+                            <div className="w-full flex flex-col py-3">
+                                <div className="bg-gray-200 h-10 animate-pulse w-20 rounded mb-2" />
+                                <div className="bg-gray-200 h-6 animate-pulse w-40 rounded" />
+                            </div>
+                            <div className="w-full py-2">
+                                <div className="rounded-full bg-gray-200 animate-pulse rounded-full w-full h-12" />
+                            </div>
+    
+                            <div className="w-full flex flex-row space-x-8 items-center">
+                                <div className="w-6/12 p-3">
+                                    <div className="rounded bg-gray-200 animate-pulse h-12" />
+                                </div>
+                                <div className="w-6/12 flex flex-row justify-between">
+                                    <span className="bg-gray-200 animate-pulse h-5 w-5 rounded-full" />
+                                    <span className="bg-gray-200 animate-pulse h-5 w-5 rounded-full" />
+                                    <span className="bg-gray-200 animate-pulse h-5 w-5 rounded-full" />
+                                    <span className="bg-gray-200 animate-pulse h-5 w-5 rounded-full" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+
     return (
         <section className="w-full bg-white">
             <div className="w-full max-w-5xl mx-auto flex flex-col px-4 py-5">
@@ -71,5 +118,5 @@ export default function Hero() {
                 </div>
             </div>
         </section>
-    )
+    );
 }
