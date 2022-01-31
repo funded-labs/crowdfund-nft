c34bdbd01c24439c23bd6a3f4a3d56e9e4f7c772
Max Lascombe <mtl2154@columbia.edu>
2022-01-31 10:08:38 -0500

    Added GA.

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,39 @@
-import Document, { Html, Main, Head, NextScript } from "next/document";
+import Document, { Html, Main, Head, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
 
-  render() {
-    return (
-      <Html>
-        <body>
-            <Head />
-          <Main />
-          <div id="modal" />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <script
+                        async
+                        src='https://www.googletagmanager.com/gtag/js?id=G-E0X9Z3JTN3'></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-E0X9Z3JTN3', {
+              page_path: window.location.pathname,
+            });`,
+                        }}
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <div id='modal' />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
 export default MyDocument
