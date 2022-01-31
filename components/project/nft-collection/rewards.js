4920da750f1ac60f6576f569707ddc2489a67994
Zahid Mahmood <zahid@zmdev.com>
2022-01-31 11:27:04 +0000

    feat: show rewards text in rewards section

--- components/project/nft-collection/rewards.js
+++ components/project/nft-collection/rewards.js
@@ -1,11 +1,34 @@
-export default function Rewards() {
+export default function Rewards({ isLoading, project }) {
+    if (isLoading) {
+        return (
+            <div className="w-full shadow p-4 rounded-2xl bg-white flex flex-col space-y-2 items-start">
+                <div className="w-16 h-6 rounded bg-gray-200 animate-pulse" />
+    
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-7/12" />
+
+                <div className="py-5" />
+
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-full" />
+                <div className="h-4 rounded bg-gray-200 animate-pulse w-4/12" />
+            </div>
+        );
+    }
+
+    const { rewards } = project;
+
     return (
-        <div id="nft-collection-rewards" className="w-full shadow p-4 rounded-2xl bg-white flex flex-col items-start">
+        <div className="w-full shadow p-4 rounded-2xl bg-white flex flex-col items-start">
             <p className="font-bold text-base text-black">
                 Rewards
             </p>
-            
-            <p className="my-20" />
+        
+            <p className="text-gray-600 my-10">
+                {rewards}
+            </p>
         </div>
     )
 }
