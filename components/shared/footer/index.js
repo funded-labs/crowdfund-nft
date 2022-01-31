9fa42b43dc81c0f41af50cf7d1ffd77bdeff384f
Zahid Mahmood <zahid@zmdev.com>
2022-01-31 11:14:36 +0000

    feat: fixed links in footer

--- components/shared/footer/index.js
+++ components/shared/footer/index.js
@@ -1,69 +1,73 @@
-const links = [
-    {
-        title: "CrowdFund NFT",
-        items: [
-            // {
-            //     label: "About",
-            //     href: "/about"
-            // },
-            {
-                label: "Terms",
-                href: "/terms"
-            },
-            {
-                label: "List your project",
-                href: "/create-a-project"
-            },
-            {
-                label: "Crowdfunding on the IC",
-                href: ""
-            },
-        ]
-    },
-    {
-        title: "Connect with us",
-        items: [
-            {
-                label: "Twitter",
-                href: "https://twitter.com/crowdfundnft"
-            },
-            {
-                label: "Discord",
-                href: "https://discord.gg/uWEbFHBEHz"
-            }
-        ]
-    }
-]
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <footer className="w-full bg-gray-100 border-t py-10 text-sm px-4 lg:px-0">
             <div className="w-full max-w-5xl mx-auto pb-8 border-b flex flex-col space-y-5 lg:space-y-10 text-gray-600 font-semibold">
                 <div className="w-full grid grid-cols-1 lg:grid-cols-4 gap-4">
-                    {links.map((column, index) => (
-                        <div key={index} className="w-full flex flex-col">
-                            <p className="text-gray-400">{column.title}</p>
-                            <ul className="leading-loose text-crayola-900">
-                                {column.items.map((link, index) => (
-                                    <li key={index} className="flex flex-row items-center">
-                                        <a
-                                            className="hover:underline cursor-pointer"
-                                            href={link.href}
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            {link.label}
-                                        </a>
-                                        {link.pillText && (
-                                            <span className="ml-1 px-1 py-px text-xs font-extralight bg-crayola-500 rounded text-white">
-                                                {link.pillText}
-                                            </span>
-                                        )}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
+                    <div className="w-full flex flex-col">
+                        <p className="text-gray-400">CrowdFund NFT</p>
+                        <ul className="leading-loose text-crayola-900">
+                            <li className="flex flex-row items-center">
+                                <Link href="/about" passHref as="/about.html">
+                                    <a
+                                        className="hover:underline cursor-pointer"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        About
+                                    </a>
+                                </Link>
+                            </li>
+                            <li className="flex flex-row items-center">
+                                <Link href="/terms" passHref as="/terms.html">
+                                    <a
+                                        className="hover:underline cursor-pointer"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Terms
+                                    </a>
+                                </Link>
+                            </li>
+                            <li className="flex flex-row items-center">
+                                <Link href="/create-a-project" passHref as="/create-a-project.html">
+                                    <a
+                                        className="hover:underline cursor-pointer"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        List your project
+                                    </a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="w-full flex flex-col">
+                        <p className="text-gray-400">Connect with us</p>
+                        <ul className="leading-loose text-crayola-900">
+                            <li className="flex flex-row items-center">
+                                <a
+                                    className="hover:underline cursor-pointer"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    href="https://twitter.com/crowdfundnft"
+                                >
+                                    Twitter
+                                </a>
+                            </li>
+                            <li className="flex flex-row items-center">
+                                <a
+                                    className="hover:underline cursor-pointer"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    href="https://discord.gg/uWEbFHBEHz"
+                                >
+                                    Discord
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="w-full flex flex-col justify-center items-end">
                     <div className="w-full lg:w-4/12 flex flex-col items-center lg:items-end">
