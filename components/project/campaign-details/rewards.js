19127f3a0d3309c003c56b3db133c0dae0ed2751
Zahid Mahmood <zahid@zmdev.com>
2022-01-31 11:16:49 +0000

    feat: created rewards section in Campaign Details

---
+++ components/project/campaign-details/rewards.js
@@ -0,0 +1,11 @@
+export default function Rewards({ project }) {
+    return (
+        <div id="nft-collection-rewards" className="w-full shadow p-4 rounded-2xl bg-white flex flex-col items-start">
+            <p className="font-bold text-base text-black">
+                Rewards
+            </p>
+            
+            <p className="my-20" />
+        </div>
+    )
+}
