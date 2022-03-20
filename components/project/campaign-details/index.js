48324a4b65e0fde5dfa26ea48db91210657ae2c4
Zahid <zahid@zmdev.com>
2022-03-20 19:12:29 +0000

    bug: fixes to Project skeleton

--- components/project/campaign-details/index.js
+++ components/project/campaign-details/index.js
@@ -7,13 +7,8 @@ export default function CampaignDetails({ isLoading, project, author }) {
     if (isLoading) {
         return (
             <div className="w-full">
-                <div className="w-full max-w-5xl mx-auto px-4 flex flex-row py-4 space-x-4">
-                    <div className="w-3/12 flex flex-col space-y-4">
-                        <Menu isLoading={isLoading} />
-                        <AuthorProfile isLoading={isLoading} />
-                    </div>
-    
-                    <div className="w-9/12 flex flex-col">
+                <div className="w-full max-w-5xl mx-auto px-4 flex flex-col md:flex-row py-4 space-y-4 md:space-y-0 md:space-x-4">    
+                    <div className="w-full flex flex-col">
                         <Story isLoading={true} />
                     </div>
                 </div>
