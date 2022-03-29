7697392ef7244d0b5978b287689c6c67f7da716c
Zahid <zahid@zmdev.com>
2022-03-29 18:44:00 +0100

    abstracted project retrieval logic in to a separate hook

--- pages/project.js
+++ pages/project.js
@@ -6,73 +6,25 @@ import { useState } from 'react'
 import CampaignDetails from '@/components/project/campaign-details'
 import NFTCollection from '@/components/project/nft-collection'
 import Footer from '@/components/shared/footer'
-import { useQuery } from 'react-query'
 import { useRouter } from 'next/router'
-import { useBackend } from '@/context/backend'
 import Faqs from '@/components/project/faqs'
 import Head from 'next/head'
-import { makeEscrowActor } from '@/ui/service/actor-locator'
+import { ProjectDetailsProvider, useProjectDetails } from '@/context/project-details'
 
-export default function ProjectDetails() {
-    const [selectedTab, setTab] = useState('campaign-details')
+export default function ProjectDetails(props) {
     const router = useRouter()
     const { projectId } = router.query
-    const { backend } = useBackend()
-    const escrowActor = makeEscrowActor()
 
-    const {
-        data: project,
-        isLoading,
-        isError,
-        isFetching,
-    } = useQuery(
-        ['project-details', projectId, backend, escrowActor],
-        async () => {
-            if (!backend) return null
-            if (!projectId) return null
-            if (!escrowActor) return null
+    return (
+        <ProjectDetailsProvider projectId={projectId}>
+            <Screen {...props} />
+        </ProjectDetailsProvider>
+    );
+}
 
-            const { project, owner } = await backend.getProjectWithOwner(
-                projectId
-            )
-
-            let stats = {
-                nftNumber: Number(project.nftVolume),
-                nftPriceE8S:
-                    Number(BigInt(project?.goal) / project?.nftVolume) *
-                    100_000_000,
-                endTime: 0,
-                nftsSold: 0,
-                openSubaccounts: 0,
-            }
-
-            if (
-                Object.keys(project?.status?.[0] || { submitted: null })[0] !==
-                'fully_funded'
-            ) {
-                const newStats = await escrowActor.getProjectStats(+project.id)
-
-                if (newStats?.nftNumber > 0) {
-                    stats = {
-                        nftNumber: Number(newStats.nftNumber),
-                        nftPriceE8S: Number(newStats.nftPriceE8S),
-                        endTime: Number(newStats.endTime),
-                        nftsSold: Number(newStats.nftsSold),
-                        openSubaccounts: Number(newStats.openSubaccounts),
-                    }
-                }
-            }
-
-            return {
-                ...project,
-                stats,
-                owner,
-            }
-        },
-        {
-            refetchOnWindowFocus: false,
-        }
-    )
+function Screen() {
+    const [selectedTab, setTab] = useState('campaign-details')
+    const { project, isLoading, isError, isFetching } = useProjectDetails();
 
     if (isLoading || isError || isFetching || !project) {
         return (
