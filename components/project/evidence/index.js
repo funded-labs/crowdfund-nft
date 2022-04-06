212060e98358eea0c5e2bf14bf5c8cdad6789d84
Zahid <zahid@zmdev.com>
2022-04-06 12:38:02 +0100

    evidence tab

--- components/project/evidence/index.js
+++ components/project/evidence/index.js
@@ -1,69 +1,79 @@
-export default function Evidence({ project }) {
+import * as prettyDate from "pretty-date";
+
+const _records = [
+    {
+        price: 0.01,
+        from: "35c9db2bff0d617f5a21007691371b3f0642ef3c435f5978be1c080a6401a742",
+        to: "26f1173fba4749253b8c3559455e189c122178e3b4f47afdc6cba83ce3f81480",
+        time: new Date()
+    },
+    {
+        price: 0.01,
+        from: "35c9db2bff0d617f5a21007691371b3f0642ef3c435f5978be1c080a6401a742",
+        to: "26f1173fba4749253b8c3559455e189c122178e3b4f47afdc6cba83ce3f81480",
+        time: new Date()
+    },
+    {
+        price: 0.01,
+        from: "35c9db2bff0d617f5a21007691371b3f0642ef3c435f5978be1c080a6401a742",
+        to: "26f1173fba4749253b8c3559455e189c122178e3b4f47afdc6cba83ce3f81480",
+        time: new Date()
+    },
+    {
+        price: 0.01,
+        from: "35c9db2bff0d617f5a21007691371b3f0642ef3c435f5978be1c080a6401a742",
+        to: "26f1173fba4749253b8c3559455e189c122178e3b4f47afdc6cba83ce3f81480",
+        time: new Date()
+    },
+    {
+        price: 0.01,
+        from: "35c9db2bff0d617f5a21007691371b3f0642ef3c435f5978be1c080a6401a742",
+        to: "26f1173fba4749253b8c3559455e189c122178e3b4f47afdc6cba83ce3f81480",
+        time: new Date()
+    }
+]
+
+export default function Evidence({ records = [] }) {
     return (
         <div className="w-full">
             <div className="w-full max-w-5xl mx-auto">
 
-                <div class="relative overflow-x-auto rounded">
-                    <table class="w-full text-sm text-left text-gray-500">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <div className="relative overflow-x-auto rounded py-10">
+                    <table className="w-full text-sm text-left text-gray-500">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                             <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Column one
+                                <th scope="col" className="px-6 py-3">
+                                    Price
                                 </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Column two
+                                <th scope="col" className="px-6 py-3">
+                                    From
                                 </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Column three
+                                <th scope="col" className="px-6 py-3">
+                                    To
                                 </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Column four
+                                <th scope="col" className="px-6 py-3">
+                                    Time
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="bg-white border-b">
-                                <td scope="row" class="px-6 py-4 whitespace-nowrap">
-                                    Value one
-                                </td>
-                                <td class="px-6 py-4">
-                                    Value two
-                                </td>
-                                <td class="px-6 py-4">
-                                    Value three
-                                </td>
-                                <td class="px-6 py-4">
-                                    Value four
-                                </td>
-                            </tr>
-                            <tr class="bg-white border-b">
-                                <td scope="row" class="px-6 py-4 whitespace-nowrap">
-                                    Value one
-                                </td>
-                                <td class="px-6 py-4">
-                                    Value to
-                                </td>
-                                <td class="px-6 py-4">
-                                    Value three
-                                </td>
-                                <td class="px-6 py-4">
-                                    Value four
-                                </td>
-                            </tr>
-                            <tr class="bg-white">
-                                <td scope="row" class="px-6 py-4 whitespace-nowrap">
-                                    Magic Mouse 2
-                                </td>
-                                <td class="px-6 py-4">
-                                    Black
-                                </td>
-                                <td class="px-6 py-4">
-                                    Accessories
-                                </td>
-                                <td class="px-6 py-4">
-                                    $99
-                                </td>
-                            </tr>
+                            {_records.map(record => (
+                                <tr className="bg-white border-b">
+                                    <td scope="row" className="px-6 py-4 flex flex-row space-x-1 shrink-0 truncate items-center font-medium text-black">
+                                        <img src="/assets/IClogo.png" className="h-4"/>
+                                        <span>{record.price} ICP</span>
+                                    </td>
+                                    <td className="px-6 py-4 truncate whitespace-nowrap">
+                                        {record.from.substring(0, 10)}...
+                                    </td>
+                                    <td className="px-6 py-4 truncate whitespace-nowrap">
+                                        {record.to.substring(0, 10)}...
+                                    </td>
+                                    <td className="px-6 py-4 truncate whitespace-nowrap">
+                                        {prettyDate.format(record.time)}
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
