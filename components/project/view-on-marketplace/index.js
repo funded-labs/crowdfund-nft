1db4163c387ec1ac8a8e527bd3341ab2b1dd35d3
Max Lascombe <mtl2154@columbia.edu>
2022-04-08 11:10:11 -0400

    Redesigned the entrepot button a bit.

--- components/project/view-on-marketplace/index.js
+++ components/project/view-on-marketplace/index.js
@@ -1,20 +1,20 @@
 export default function ViewOnMarketplace({ project }) {
-    const { entrepotLink } = project;
-    
+    const { entrepotLink } = project
+
     // @todo: checks here to ensure project.status is fully_funded
 
     return (
-        <div className="w-full flex flex-col space-y-2">
+        <div className='w-full flex flex-col space-y-2'>
             <a
-                href="https://entrepot.app/marketplace"
-                target="_blank"
-                className={`
-                    h-14 bg-[#00d092] py-1 px-2 rounded-lg overflow-hidden
-                    flex flex-row justify-center hover:opacity-75 cursor-pointer
-                `}
-            >
-                <img src="/assets/entrepot-logo.png" className="object-contain h-full" />
+                href='https://entrepot.app/marketplace'
+                target='_blank'
+                className='h-12 bg-[#00d092] mx-10 py-1 px-2 rounded-lg overflow-hidden flex flex-row justify-center hover:opacity-75 cursor-pointer leading-10 font-bold text-sm'>
+                Trade on
+                <img
+                    src='/assets/entrepot-logo.png'
+                    className='object-contain h-full py-1 ml-2'
+                />
             </a>
         </div>
-    );
+    )
 }
