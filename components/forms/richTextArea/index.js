19f4e100e2022a856a0b75695faa95bd1adc20b6
Zahid <zahid@zmdev.com>
2022-08-23 13:26:02 +0100

    updated react-quill implementation to support images

--- components/forms/richTextArea/index.js
+++ components/forms/richTextArea/index.js
@@ -1,18 +1,53 @@
 import dynamic from 'next/dynamic'
+import { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css'
 
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false })
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
-export default function RichTextArea({ onChange, value, name, label }) {
+const ALL_TOOLBAR_OPTIONS = [
+  ['bold', 'italic', 'underline', 'strike'],          // custom button values
+  [{ 'list': 'ordered' }, { 'list': 'bullet' }],        // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+  ['clean'],                                         // remove formatting button
+  ['image']
+];
+
+export default function RichTextArea({ onChange, value, name, label, exclude = [] }) {
+  console.log({exclude, ALL_TOOLBAR_OPTIONS});
+  const TOOLBAR_OPTIONS = ALL_TOOLBAR_OPTIONS.filter(o => {
+    return o.some(x => !exclude.includes(x));
+});
+  
   return (
-      <div className="w-full">
-          {label && (
-              <label htmlFor={name} className="text-xs text-gray-400">
-                  {props.label}
-              </label>
-          )}
+    <div className="w-full">
+      {label && (
+        <label htmlFor={name} className="text-xs text-gray-400">
+          {props.label}
+        </label>
+      )}
 
-        <ReactQuill name={name} onChange={onChange} value={value} className="h-[300px] mb-[60px]" theme="snow" />
-      </div>
+      <ReactQuill
+        name={name}
+        onChange={onChange}
+        value={value}
+        className="h-[300px] mb-[60px]"
+        theme="snow"
+        modules={{
+          toolbar: {
+            container: TOOLBAR_OPTIONS,
+            handlers: {
+              image: function (value) {
+                var range = this.quill.getSelection();
+                var value = prompt('Enter the image URL here');
+                if(value){
+                    this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
+                }
+              }
+            }
+          }
+        }}
+      />
+    </div>
   )
 }
