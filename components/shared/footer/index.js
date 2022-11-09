6f00189dd3c86a3038d1c38d2516579124228e6b
Mazen Zibara <mazen.zibara@makit.lv>
2022-11-09 16:00:20 +0200

    Fix links

--- components/shared/footer/index.js
+++ components/shared/footer/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import icbadge from '../../../public/assets/icbadge.svg'
 
 const navigation = {
@@ -8,25 +9,23 @@ const navigation = {
       
     ],
     support: [
-      { name: 'Creator guide', href: '/p/project-creator-guide' },
-      
-      
+      { name: 'Creator guide', href: '/p/project-creator-guide', as: '/p/project-creator-guide.html' },
     ],
     company: [
-      { name: 'About', href: '/about' },
+      { name: 'About', href: '/about', as: '/about.html' },
       { name: 'Blog', href: 'https://medium.com/@luke_71499/crowdfund-nft-an-alternative-to-traditional-crowdfunding-using-nfts-89ef15bd65d8' },
       
     ],
     legal: [
-      { name: 'Terms & conditions', href: '/p/terms' },
-      { name: 'Privacy policy', href: '/p/privacy' },
-      { name: 'Marketplace terms', href: '/p/marketplaceterms' },
+      { name: 'Terms & conditions', href: '/p/terms', as: '/p/terms.html' },
+      { name: 'Privacy policy', href: '/p/privacy', as: '/p/privacy.html' },
+      { name: 'Marketplace terms', href: '/p/marketplaceterms', as: '/p/marketplaceterms.html' },
     ],
     social: [
       
       {
         name: 'Twitter',
-        href: '#',
+        href: 'https://twitter.com/crowdfundnft',
         icon: (props) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -35,7 +34,7 @@ const navigation = {
       },
       {
         name: 'GitHub',
-        href: '#',
+        href: 'https://github.com/CrowdFund-NFT',
         icon: (props) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path
@@ -104,9 +103,11 @@ const navigation = {
                   <ul role="list" className="mt-4 space-y-4">
                     {navigation.support.map((item) => (
                       <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                          {item.name}
-                        </a>
+                        <Link href={item.href} as={item.as}>
+                          <a className="text-base text-gray-500 hover:text-gray-900">
+                            {item.name}
+                          </a>
+                        </Link>
                       </li>
                     ))}
                   </ul>
@@ -118,9 +119,11 @@ const navigation = {
                   <ul role="list" className="mt-4 space-y-4">
                     {navigation.company.map((item) => (
                       <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                          {item.name}
-                        </a>
+                        <Link href={item.href} as={item.as || item.href}>
+                          <a className="text-base text-gray-500 hover:text-gray-900">
+                            {item.name}
+                          </a>
+                        </Link>
                       </li>
                     ))}
                   </ul>
@@ -130,9 +133,11 @@ const navigation = {
                   <ul role="list" className="mt-4 space-y-4">
                     {navigation.legal.map((item) => (
                       <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                          {item.name}
-                        </a>
+                        <Link href={item.href} as={item.as}>
+                          <a className="text-base text-gray-500 hover:text-gray-900">
+                            {item.name}
+                          </a>
+                        </Link>
                       </li>
                     ))}
                   </ul>
