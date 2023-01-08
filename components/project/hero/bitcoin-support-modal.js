7e761164a2afe21a721cc112c6ee52ba1d3b2f47
Mazen Zibara <mazen.zibara@makit.lv>
2023-01-08 11:30:33 +0200

    Further

--- components/project/hero/bitcoin-support-modal.js
+++ components/project/hero/bitcoin-support-modal.js
@@ -1,48 +1,144 @@
+import { Spinner } from '@/components/shared/loading-spinner'
 import Modal from '@/components/shared/modal'
-import { useState } from 'react'
+import { createActor } from '@/helpers/createActor'
+import { Principal } from '@dfinity/principal'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { create } from 'react-modal-promise'
 import QRCode from "react-qr-code"
 
-const BitcoinSupportModal = ({ isOpen, onResolve, onReject } ) => {
+export const idlFactory = ({ IDL }) => {
+  const AccountIdText = IDL.Text
+  const Result_1 = IDL.Variant({ ok: IDL.Null, err: IDL.Text })
+  const Result_2 = IDL.Nat64
+  const Result = IDL.Variant({ ok: AccountIdText, err: IDL.Text })
+  return IDL.Service({
+    cancelTransfer: IDL.Func([AccountIdText], [], []),
+    confirmTransfer: IDL.Func([AccountIdText], [Result_1], []),
+    getNewAccountId: IDL.Func([IDL.Principal, IDL.Nat, IDL.Text], [Result], []),
+    accountBalance: IDL.Func([IDL.Text, IDL.Text], [Result_2], [])
+  })
+}
+
+const BitcoinSupportModal = ({ isOpen, wallet, project, launchDate, status, canisterPrincipal, selectedTier, sum, onResolve, onReject } ) => {
   const [walletAddress, setWalletAddress] = useState()
-  const [loading, setLoading] = useState()
+  const [loadingLabel, setLoadingLabel] = useState()
+  const intervalRef = useRef()
 
-  const renderFundButton = () => {
-    const action = walletAddress ? markBTCAsPaid : backProjectButtonClick
-    const actionLabel = walletAddress ? 'I have transfered funds' : 'Fund this project'
+  const price8s = useMemo(() => {
+    return project.stats?.nftStats[selectedTier]?.priceSatoshi || 0
+  }, [project, selectedTier])
 
+  const price = useMemo(() => {
+    return price8s / 100000000
+  }, [price8s])
+
+  const escrowActor = useMemo(() => {
+    return createActor(canisterPrincipal[0], idlFactory)
+  }, [canisterPrincipal])
+
+  useEffect(() => {
+    if (!escrowActor || !wallet) return
+
+    setLoadingLabel('Obtaining BTC address')
+    escrowActor
+      .getNewAccountId(Principal.from(wallet.id), selectedTier, "BTC")
+      .then((result) => setWalletAddress(result.ok))
+      .catch(onReject)
+      .finally(() => setLoadingLabel())
+
+  }, [escrowActor, wallet])
+
+  useEffect(() => {
+    return () => { clearInterval(intervalRef.current) }
+  }, [intervalRef])
+
+  const setCheckTransactionInterval = useCallback(() => {
+    intervalRef.current = setInterval(() => {
+      escrowActor.accountBalance(walletAddress, 'BTC')
+        .then(result => {
+          const walletBalance = Number(result)
+          if (walletBalance == 0) return
+
+          if (walletBalance == price8s) {
+            onResolve()
+            return
+          }
+
+          if (walletBalance < price8s) {
+            setLoadingLabel(`Transfer received, however it was ${price8s - walletBalance} BTC short`)
+          }
+        })
+        .catch(console.log)
+    }, 30 * 1000)
+  }, [escrowActor, walletAddress, price8s, setLoadingLabel])
+
+  const markBTCAsPaid = useCallback(() => {
+    setLoadingLabel('Marking payment as processed by user')
+
+    escrowActor
+      .confirmTransfer(walletAddress)
+      .then(() => {
+        setLoadingLabel('Waiting for payment confirmation.\nUsually it takes around 10 minutes to receive first confirmation')
+        setCheckTransactionInterval()
+      })
+      .catch(onReject)
+  }, [escrowActor, walletAddress])
+
+  const renderConfirmButton = () => {
     return (
         <button
             disabled={
-                loading || project.stats.endTime <= 0
+              project.stats.endTime <= 0
             }
             className={`
-            flex flex-row justify-center bg-gradient-to-b from-neutral-800 to-black cursor-pointer text-white text-lg 
-            font-medium rounded-xl w-full appearance-none focus:outline-none py-5 
-            px-4 hover:bg-blue-700
-        `}
+              flex flex-row justify-center bg-gradient-to-b from-neutral-800 to-black cursor-pointer text-white text-lg 
+              font-medium rounded-xl w-full appearance-none focus:outline-none py-5 
+              px-4 hover:bg-blue-700
+            `}
             type='button'
-            onClick={action}>
-            {loading ? (
-                <span className='h-5 w-5'>
-                    <Spinner show={true} />
-                </span>
-            ) : launchDate &&
-                launchDate !== '' &&
-                status === 'approved' ? (
-                <>Launching on {launchDate}</>
-            ) : (
-                <>{actionLabel}</>
-            )}
+            onClick={markBTCAsPaid}
+        >
+          <>I have transfered funds</>
         </button>
     )
-}
+  }
 
+  const renderBtcAddress = () => (
+    <div className='flex flex-col items-center space-y-6'>
+      <div className='text-center text-gray-600 font-medium'>BTC DEPOSIT ADDRESS</div>
+      {walletAddress && <QRCode value={`bitcoin:${walletAddress}?amount=${price}`} className='h-40 w-40'/>}
+      <div className='text-center text-blue-600 font-medium'>{walletAddress}</div>
+      <div className='text-center mb-2 text-gray-600 font-bold'>Please transfer exact amount of {price} BTC</div>
+    </div>
+  )
+
+  const renderLoading = () => (
+    <div className='flex flex-col items-center'>
+      <div className='h-8 w-8'>
+        <Spinner show={true} />
+      </div>
+      <div className='mt-3 text-center text-gray-600 font-medium'>{loadingLabel}</div>
+    </div>
+  )
 
   return (
-    <Modal show={isOpen} size="sn" width="100%" height="100%">
-      {walletAddress && <QRCode value={walletAddress}/>}
-      {renderFundButton()}
+    <Modal show={isOpen} size="static" width="100%" height="100%" transparent>
+      <div className='w-full h-full p-6 rounded-md mx-auto bg-white'>
+        <div className="flex flex-1 p-4 border-2 border-gray-200 h-full rounded-md mx-auto flex flex-col items-center">
+          <div className='flex flex-row items-center'>
+            <img src='assets/bitcoin.svg' />
+            <div className='flex flex-row items-end ml-2'>
+              <div className='text-5xl font-medium'>{price}</div>
+              <div className='text-gray-600 text-2xl ml-2'>BTC</div>
+            </div>
+          </div>
+          <div className='flex flex-1 items-center'>
+            {!loadingLabel && walletAddress && renderBtcAddress()}
+            {loadingLabel && renderLoading()}
+          </div>
+          {!loadingLabel && walletAddress && renderConfirmButton()}
+        </div>
+      </div>
     </Modal>
   )
 }
---
+++ helpers/createActor.js
@@ -0,0 +1,26 @@
+import { Actor, HttpAgent } from '@dfinity/agent'
+
+export const createActor = (canisterId, idlFactory) => {
+  const agent = new HttpAgent({
+      host:
+          process.env.NODE_ENV === 'production'
+              ? 'https://ic0.app'
+              : 'http://127.0.0.1:8000/',
+  })
+
+  // Fetch root key for certificate validation during development
+  if (process.env.NODE_ENV !== 'production') {
+      agent.fetchRootKey().catch((err) => {
+          console.warn(
+              'Unable to fetch root key. Check to ensure that your local replica is running'
+          )
+          console.error(err)
+      })
+  }
+
+  // Creates an actor with using the candid interface and the HttpAgent
+  return Actor.createActor(idlFactory, {
+      agent,
+      canisterId,
+  })
+}
